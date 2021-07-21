213bf5a8665298c48e35e70057a633603a4324f8
ivandevyo <ivandevyo@quartsoft.com>
2021-07-21 21:10:45 +0300

    fix code style

--- main.test.js
+++ main.test.js
@@ -31,17 +31,17 @@ test('getPath return valid string when element have siblings', () => {
 test('getPath return valid string when element have no siblings', () => {
     document.body.innerHTML =
         '<header class="header">' +
-        '<article class="article"></article>' +
-        '<article class="article article--modified">' +
-        '<div class="block-one" data-set="1"></div>' +
-        '<div class="block-one1" data-set="1"></div>' +
-        '<div class="block-one" data-set="2"></div>' +
-        '<div class="block-one" data-set="3"></div>' +
-        '<div class="block-one" data-set="4"></div>' +
-        '</article>' +
-        '<article class="article article--super-modified">' +
-        '<div class="block-one"></div>' +
-        '</article>' +
+            '<article class="article"></article>' +
+            '<article class="article article--modified">' +
+                '<div class="block-one" data-set="1"></div>' +
+                '<div class="block-one1" data-set="1"></div>' +
+                '<div class="block-one" data-set="2"></div>' +
+                '<div class="block-one" data-set="3"></div>' +
+                '<div class="block-one" data-set="4"></div>' +
+            '</article>' +
+            '<article class="article article--super-modified">' +
+                '<div class="block-one"></div>' +
+            '</article>' +
         '</header>'
     const element = document.querySelectorAll('header .article--super-modified .block-one')[0]
     const string = getPath(element)
