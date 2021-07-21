b2bc508a236cbedfc3c20cd2de1246c8851662ca
ivandevyo <ivandevyo@quartsoft.com>
2021-07-21 19:16:14 +0300

    Added getAttributeString function

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 window.getPath = function (el) {
-    
+    let string = getAttributeString(el)
+    // coming soon
+    return string
+}
+
+function getAttributeString(el) {
+    const attributes = el.attributes
+    let string = ''
+    for (let i = 0; i < attributes.length; i++) {
+        const nodeItem = attributes[i]
+        switch (nodeItem.nodeName) {
+            case 'class': string += '.'; break
+            case 'id': string += '#'; break
+        }
+        string += nodeItem.value
+    }
+    if (!string) return el.tagName
+    return string
 }
